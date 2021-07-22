56518bc6c7a68abf9bd69e887051a63b2bee61ea
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-07-22 20:57:37 +0300

    disable favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,13 +24,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
     <link href="{{ url(mix('css/app.css')) }}" rel="stylesheet">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+    {{--<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
     <link rel="manifest" href="/images/favicons/site.webmanifest">
     <link rel="shortcut icon" href="/images/favicons/favicon.ico">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="/images/favicons/browserconfig.xml">
+    <meta name="msapplication-config" content="/images/favicons/browserconfig.xml">--}}
     <meta name="theme-color" content="#ffffff">
 </head>
 <body>
